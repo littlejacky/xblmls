f4b0df5af1549566ad30f40927360520b0003928
xibaluma-xblm <xunbaolei@sina.cn>
2024-11-11 17:54:23 +0800

    submit

--- src/XBLMS.Web/wwwroot/sitefiles/assets/js/admin/study/studyPlanManager.js
+++ src/XBLMS.Web/wwwroot/sitefiles/assets/js/admin/study/studyPlanManager.js
@@ -32,6 +32,142 @@ var data = utils.init({
     keywords: '',
   },
   courseList: null,
+  passSeries: [100],
+  passChartOptions: {
+    chart: {
+      type: 'radialBar',
+      toolbar: {
+        show: false
+      }
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24
+          }
+        },
+        track: {
+          background: '#fff',
+          strokeWidth: '88%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35
+          }
+        },
+
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -20,
+            show: true,
+            color: '#ff6a00',
+            fontSize: '16px'
+          },
+          value: {
+            formatter: function (val) {
+              return val + '%';
+            },
+            color: '#000',
+            fontSize: '36px',
+            show: true,
+          }
+        }
+      }
+    },
+    fill: {
+      colors: ['#67C23A']
+    },
+    stroke: {
+      lineCap: 'round'
+    },
+    labels: ['完成率'],
+  },
+  pass1Series: [100],
+  pass1ChartOptions: {
+    chart: {
+      type: 'radialBar',
+      toolbar: {
+        show: false
+      }
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24
+          }
+        },
+        track: {
+          background: '#fff',
+          strokeWidth: '88%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35
+          }
+        },
+
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -20,
+            show: true,
+            color: '#ff6a00',
+            fontSize: '16px'
+          },
+          value: {
+            formatter: function (val) {
+              return val + '%';
+            },
+            color: '#000',
+            fontSize: '36px',
+            show: true,
+          }
+        }
+      }
+    },
+    fill: {
+      colors: ['#67C23A']
+    },
+    stroke: {
+      lineCap: 'round'
+    },
+    labels: ['达标率'],
+  },
 });
 
 var methods = {
