00a0e9e2d99b434c0f73687490b3a34fa4cc087b
xblms <xunbaolei@sina.cn>
2024-12-17 16:01:48 +0800

    bugfix Redis下两次登录问题

--- src/XBLMS.Web/wwwroot/sitefiles/assets/js/app/login.js
+++ src/XBLMS.Web/wwwroot/sitefiles/assets/js/app/login.js
@@ -1,7 +1,5 @@
 var $url = '/login';
 var $urlCaptcha = '/login/captcha';
-var $urlCaptchaCheck = '/login/captcha/actions/check';
-var $urlSendSms = '/login/actions/sendSms';
 
 var data = utils.init({
   status: utils.getQueryInt('status'),
@@ -11,10 +9,8 @@ var data = utils.init({
   isUserCaptchaDisabled: false,
   countdown: 0,
   form: {
-    type: 'account',
     account: null,
     password: null,
-    mobile: null,
     isPersistent: false,
     captchaValue: null,
   },
@@ -67,45 +63,23 @@ var methods = {
     });
   },
 
-  apiCaptchaCheck: function () {
-    var $this = this;
-    if (this.isUserCaptchaDisabled) {
-      $this.apiSubmit();
-    } else {
-      utils.loading(this, true);
-      $api.post($urlCaptchaCheck, {
-        token: this.captchaToken,
-        value: this.form.captchaValue
-      }).then(function (response) {
-        $this.apiSubmit();
-      }).catch(function (error) {
-        $this.apiCaptcha();
-        utils.loading($this, false);
-        utils.error(error);
-      });
-    }
-  },
-
   apiSubmit: function () {
     var $this = this;
 
     utils.loading(this, true);
     $api.post($url, {
-      appLogin:true,
+      appLogin: true,
       account: this.form.account,
       password: this.form.password ? md5(this.form.password) : '',
-      mobile: this.form.mobile,
+      token: this.captchaToken,
+      value: this.form.captchaValue,
       isPersistent: this.form.isPersistent
     }).then(function (response) {
       var res = response.data;
 
       localStorage.removeItem(ACCESS_TOKEN_NAME);
       localStorage.setItem(ACCESS_TOKEN_NAME, res.token);
-      if (res.redirectToVerifyMobile) {
-        location.href = utils.getRootUrl('verifyMobile', {
-          returnUrl: $this.returnUrl
-        });
-      } else if ($this.returnUrl) {
+      if ($this.returnUrl) {
         location.href = $this.returnUrl;
       } else {
         $this.redirectIndex();
@@ -126,41 +100,22 @@ var methods = {
     var $this = this;
 
     this.$refs.formAccount && this.$refs.formAccount.clearValidate();
-    this.$refs.formMobile && this.$refs.formMobile.clearValidate();
-    if (this.form.type == 'account') {
-      setTimeout(function () {
-        $this.$refs['account'].focus();
-      }, 100);
-    } else if (this.form.type == 'mobile') {
-      setTimeout(function () {
-        $this.$refs['mobile'].focus();
-      }, 100);
-    }
+    setTimeout(function () {
+      $this.$refs['account'].focus();
+    }, 100);
   },
 
   btnCaptchaClick: function () {
     this.apiCaptcha();
   },
 
-  isMobile: function (value) {
-    return /^1[3-9]\d{9}$/.test(value);
-  },
   btnSubmitClick: function () {
     var $this = this;
-
-    if (this.form.type == 'account') {
-      this.$refs.formAccount.validate(function (valid) {
-        if (valid) {
-          $this.apiCaptchaCheck();
-        }
-      });
-    } else {
-      this.$refs.formMobile.validate(function (valid) {
-        if (valid) {
-          $this.apiSubmit();
-        }
-      });
-    }
+    this.$refs.formAccount.validate(function (valid) {
+      if (valid) {
+        $this.apiSubmit();
+      }
+    });
   },
 
 };
@@ -170,7 +125,7 @@ var $vue = new Vue({
   data: data,
   methods: methods,
   created: function () {
-    document.title = '登 录';
+    document.title = DOCUMENTTITLE_HOME + '-登录';
     this.apiGet();
   }
 });
