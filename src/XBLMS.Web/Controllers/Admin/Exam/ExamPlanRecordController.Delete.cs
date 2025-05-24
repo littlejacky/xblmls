f26ab8bb163f878587826ed3ffb7551ce8a86b87
littlejacky <littlejacky@126.com>
2025-05-24 09:56:16 +0800

    update

---
+++ src/XBLMS.Web/Controllers/Admin/Exam/ExamPlanRecordController.Delete.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+using XBLMS.Dto;
+using XBLMS.Enums;
+using XBLMS.Utils;
+namespace XBLMS.Web.Controllers.Admin.Exam
+{
+    public partial class ExamPlanRecordController
+    {
+        [HttpPost, Route(RouteDelete)]
+        public async Task<ActionResult<BoolResult>> Delete([FromBody] IdRequest request)
+        {
+            if (!await _authManager.HasPermissionsAsync(MenuPermissionType.Delete))
+            {
+                return this.NoAuth();
+            }
+            var plan = await _examPlanRecordRepository.GetAsync(request.Id);
+            if (plan != null)
+            {
+                await _examPlanRecordRepository.DeleteAsync(plan.Id);
+                //await _examManager.ClearRandom(paper.Id, true);
+
+                await _authManager.AddAdminLogAsync("删除考试计划", $"{plan.Title}");
+                await _authManager.AddStatLogAsync(StatType.ExamPlanDelete, "删除考试计划", plan.Id, plan.Title);
+                await _authManager.AddStatCount(StatType.ExamPlanDelete);
+
+            }
+            return new BoolResult
+            {
+                Value = true
+            };
+        }
+
+    }
+}
---
+++ src/XBLMS.Web/Controllers/Admin/Exam/ExamPlanRecordController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using System.Collections.Generic;
+using XBLMS.Configuration;
+using XBLMS.Models;
+using XBLMS.Repositories;
+using XBLMS.Services;
+
+namespace XBLMS.Web.Controllers.Admin.Exam
+{
+    [OpenApiIgnore]
+    [Authorize(Roles = Types.Roles.Administrator)]
+    [Route(Constants.ApiAdminPrefix)]
+    public partial class ExamPlanRecordController : ControllerBase
+    {
+        private const string Route = "exam/examPlanRecord";
+        private const string RouteDelete = Route + "/del";
+        private const string RouteLock = Route + "/lock";
+        private const string RouteUnLock = Route + "/unLock";
+
+        private readonly IAuthManager _authManager;
+        private readonly IExamManager _examManager;
+        private readonly IExamPlanRecordRepository _examPlanRecordRepository;
+        private readonly IExamPaperTreeRepository _examPaperTreeRepository;
+        private readonly IExamPaperUserRepository _examPaperUserRepository;
+        private readonly IExamPaperStartRepository _examPaperStartRepository;
+        private readonly IStudyCourseRepository _studyCourseRepository;
+        private readonly IStudyPlanCourseRepository _studyPlanCourseRepository;
+        public ExamPlanRecordController(IAuthManager authManager,
+            IExamManager examManager,
+            IExamPlanRecordRepository examPlanRecordRepository,
+            IExamPaperTreeRepository examPaperTreeRepository,
+            IExamPaperUserRepository examPaperUserRepository,
+            IExamPaperStartRepository examPaperStartRepository,
+            IStudyCourseRepository studyCourseRepository,
+            IStudyPlanCourseRepository studyPlanCourseRepository)
+        {
+            _authManager = authManager;
+            _examManager = examManager;
+            _examPlanRecordRepository = examPlanRecordRepository;
+            _examPaperTreeRepository = examPaperTreeRepository;
+            _examPaperUserRepository = examPaperUserRepository;
+            _examPaperStartRepository = examPaperStartRepository;
+            _studyCourseRepository = studyCourseRepository;
+            _studyPlanCourseRepository = studyPlanCourseRepository;
+        }
+        public class GetLockedRequest
+        {
+            public int Id { get; set; }
+            public bool Locked { get; set; }
+        }
+        public class GetRequest
+        {
+            public bool TreeIsChildren { get; set; }
+            public int TreeId { get; set; }
+            public string Keyword { get; set; }
+            public int PageIndex { get; set; }
+            public int PageSize { get; set; }
+        }
+        public class GetResult
+        {
+            public List<ExamPlanRecord> Items { get; set; }
+            public int Total { get; set; }
+
+        }
+    }
+}
